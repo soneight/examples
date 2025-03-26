e51ee901a1c259ef515524d9609f0ffb77488abd
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-03-26 23:46:13 +0200

    overglad redbook1 1_3 finish

--- source/overglad/redbook1/1_3.cxx
+++ source/overglad/redbook1/1_3.cxx
@@ -1,49 +1,87 @@
 #include <header.hxx>
 #define SON8_OVERGLAD_INCLUDE_DEPRECATED
 #include <son8/overglad/v1_1.hxx>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <optional>
 
 static GLfloat spin = 0.0;
 
 void display();
 void spinDisplay();
 
+void myInit();
+void myReshape( GLsizei w, GLsizei h );
+
 int main()
 {
+    using OptSpin = std::optional< bool >;
     app::Config config{
         SON8_OVERGLAD_VERSION,
         app::Config::Height{ 500u },app::Config::Width{ 500u }
     };
     app::Windowed window{ config };
 
-    glClearColor( 0.0, 0.0, 0.0, 1.0) ;
-    glColor3f( 1.0, 1.0, 1.0 );
-    glShadeModel( GL_FLAT );
+    myInit();
+    myReshape( config.width(), config.height() );
 
-    glViewport( 0, 0, config.width(), config.height() );
-    gl::matrix_mode( gl::enums::MatrixMode::Project );
-    gl::load_identity( );
-    gl::ortho( -50.0, 50.0, -50.0, 50.0, -1.0, 1.0 );
-    gl::matrix_mode( gl::enums::MatrixMode::Model );
-    gl::load_identity( );
+    glfwSetFramebufferSizeCallback( window, []( GLFWwindow *, int w, int h ) {
+        myReshape( w, h );
+    } );
 
-    window.run( spinDisplay );
-}
+    static auto spinning = []( OptSpin set = std::nullopt ) {
+        static bool spinning_ = false;
+        if ( !set ) return spinning_;
+        spinning_ = set.value();
+        return spinning_;
+    };
 
+    glfwSetMouseButtonCallback( window, []( GLFWwindow *, int button, int action, int ) {
+        if ( button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS ) spinning( true );
+        if ( button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS ) spinning( false );
+    } );
+
+    window.run( [ ]( ) {
+        spinning( ) ? spinDisplay() : display();
+    } );
+} // main
 
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    gl::clear( gl::enums::Clearbit::Color);
 
-    glPushMatrix();
+    gl::push_matrix();
         glRotatef(spin, .0f, .0f, 1.f);
-        glRectf(-25.f, -25.f, 25.f, 25.f);
-    glPopMatrix();
+        gl::rect(-25.f, -25.f, 25.f, 25.f);
+    gl::pop_matrix();
 
-    glFlush();
+    gl::flush();
 }
 
 void spinDisplay() {
     spin += 2.0;
     if (spin > 360.0) spin = 0.0;
-    display();
+    display( );
+}
+
+void myInit()
+{
+    gl::clear_color( );
+    gl::color( 1.f, 1.f, 1.f );
+    glShadeModel( GL_FLAT );
+}
+
+void myReshape( GLsizei w, GLsizei h )
+{
+    auto divw = h / static_cast< GLfloat >( w );
+    auto divh = w / static_cast< GLfloat >( h );
+    gl::viewport( 0, 0, w, h );
+    gl::matrix_mode( gl::enums::MatrixMode::Project );
+    gl::load_identity( );
+    (w <= h)
+    ? gl::ortho( -50.0, 50.0, -50.0 * divw, 50.0 * divw, -1.0, 1.0 )
+    : gl::ortho( -50.0 * divh, 50.0 * divh, -50.0, 50.0, -1.0, 1.0 );
+    gl::matrix_mode( gl::enums::MatrixMode::Model );
+    gl::load_identity( );
 }
